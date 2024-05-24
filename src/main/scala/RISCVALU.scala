0f61abf6d58534f200e422092ace9fa118fe28d0
FaizanAhmadKhan96 <faizanahmadkhan96@gmail.com>
2024-05-24 12:44:32 +0300

    Final changes

--- src/main/scala/RISCVALU.scala
+++ src/main/scala/RISCVALU.scala
@@ -62,30 +62,36 @@ class PExtALU extends Module {
   val Bup_16 = io.operand_B(31,16)
   val Blow_16 = io.operand_B(15,0)
 
-  val result_32 = Wire(SInt(32.W))
-  val resultup_16 = Wire(SInt(16.W))
-  val resultlow_16 = Wire(SInt(16.W))       
+  val result_32 = Reg(SInt(32.W))
+  val resultup_16 = Reg(SInt(16.W))
+  val resultlow_16 = Reg(SInt(16.W))       
 
   result_32 := 0.S
   resultup_16 := 0.S
   resultlow_16 := 0.S
 
+  val two_32 = Module(new TwosComplement(32))
+  val twolow_16 = Module(new TwosComplement(16))
+  val twoup_16 = Module(new TwosComplement(16))
+
+  val Add = Module(new Adder(32))
+  val Addlow_16 = Module(new Adder(16))
+  val Addup_16 = Module(new Adder(16))
+
+   // Initialize all module inputs to avoid uninitialized reference errors
+  two_32.io.B_in := 0.U
+  twolow_16.io.B_in := 0.U
+  twoup_16.io.B_in := 0.U
+  Add.io.A_in := 0.U
+  Add.io.B_in := 0.U
+  Addlow_16.io.A_in := 0.U
+  Addlow_16.io.B_in := 0.U
+  Addup_16.io.A_in := 0.U
+  Addup_16.io.B_in := 0.U
+
     switch(io.ALU_SEL) {
       is(AluOP.ADD32) {
 
-        //val Addlow = Module(new Adder16bits)
-        //Addlow.io.A_in := Alow_16
-        //Addlow.io.B_in := Blow_16
-        //Addlow.io.carry_in := 0.U
-        //resultlow_16 := Addlow.io.sum 
-//
-        //val Addup = Module(new Adder16bits)
-        //Addup.io.A_in := Aup_16
-        //Addup.io.B_in := Bup_16
-        //Addup.io.carry_in := Addlow.io.carry_out
-        //resultup_16 := Addup.io.sum 
-
-        val Add = Module(new Adder(32))
         Add.io.A_in := A_32
         Add.io.B_in := B_32
 
@@ -94,12 +100,8 @@ class PExtALU extends Module {
 
       is(AluOP.SUB32) {
 
-        //result_32 := (A_32 + (~(B_32) + 1.U)).asSInt
-
-        val two_32 = Module(new TwosComplement(32))
         two_32.io.B_in := B_32
 
-        val Add = Module(new Adder(32))
         Add.io.A_in := A_32
         Add.io.B_in := two_32.io.B_out
 
@@ -108,76 +110,69 @@ class PExtALU extends Module {
 
       is(AluOP.ADD16) {
 
-        val Addlow = Module(new Adder(16))
-        Addlow.io.A_in := Alow_16
-        Addlow.io.B_in := Blow_16
-        resultlow_16  := Addlow.io.sum
+        Addlow_16.io.A_in := Alow_16
+        Addlow_16.io.B_in := Blow_16
+        resultlow_16  := Addlow_16.io.sum
 
-        val Addup = Module(new Adder(16))
-        Addup.io.A_in := Aup_16
-        Addup.io.B_in := Bup_16
-        resultup_16  := Addup.io.sum
+        Addup_16.io.A_in := Aup_16
+        Addup_16.io.B_in := Bup_16
+        resultup_16  := Addup_16.io.sum
 
         result_32 := Cat(resultup_16,resultlow_16).asSInt
       }
 
       is(AluOP.SUB16) {
 
-        val two_16low = Module(new TwosComplement(16))
-        two_16low.io.B_in := Blow_16
+        twolow_16.io.B_in := Blow_16
 
-        val Addlow = Module(new Adder(16))
-        Addlow.io.A_in := Alow_16
-        Addlow.io.B_in := two_16low.io.B_out
-        resultlow_16  := Addlow.io.sum
+        Addlow_16.io.A_in := Alow_16
+        Addlow_16.io.B_in := twolow_16.io.B_out
+        resultlow_16  := Addlow_16.io.sum
 
-        val two_16up = Module(new TwosComplement(16))
-        two_16up.io.B_in := Bup_16
+        twoup_16.io.B_in := Bup_16
 
-        val Addup = Module(new Adder(16))
-        Addup.io.A_in := Aup_16
-        Addup.io.B_in := two_16up.io.B_out
-        resultup_16  := Addup.io.sum
+        Addup_16.io.A_in := Aup_16
+        Addup_16.io.B_in := twoup_16.io.B_out
+        resultup_16  := Addup_16.io.sum
 
        result_32 := Cat(resultup_16,resultlow_16).asSInt
       }
 
       is(AluOP.ADSUBC16) {
 
-        val two_16up = Module(new TwosComplement(16))
-        two_16up.io.B_in := Bup_16
+        twolow_16.io.B_in := Bup_16
 
-        val Addlow = Module(new Adder(16))
-        Addlow.io.A_in := Alow_16
-        Addlow.io.B_in := two_16up.io.B_out
-        resultlow_16  := Addlow.io.sum
+        Addlow_16.io.A_in := Alow_16
+        Addlow_16.io.B_in := twolow_16.io.B_out
+        resultlow_16  := Addlow_16.io.sum
 
-        val Addup = Module(new Adder(16))
-        Addup.io.A_in := Aup_16
-        Addup.io.B_in := Blow_16
-        resultup_16  := Addup.io.sum
+        Addup_16.io.A_in := Aup_16
+        Addup_16.io.B_in := Blow_16
+        resultup_16  := Addup_16.io.sum
 
         result_32 := Cat(resultup_16,resultlow_16).asSInt
       }
 
       is(AluOP.SUBADC16) {
 
-        val Addlow = Module(new Adder(16))
-        Addlow.io.A_in := Alow_16
-        Addlow.io.B_in := Bup_16
-        resultlow_16  := Addlow.io.sum
+        Addlow_16.io.A_in := Alow_16
+        Addlow_16.io.B_in := Bup_16
+        resultlow_16  := Addlow_16.io.sum
 
-        val two_16up = Module(new TwosComplement(16))
-        two_16up.io.B_in := Blow_16
+        twoup_16.io.B_in := Blow_16
 
-        val Addup = Module(new Adder(16))
-        Addup.io.A_in := Aup_16
-        Addup.io.B_in := two_16up.io.B_out
-        resultup_16  := Addup.io.sum
+        Addup_16.io.A_in := Aup_16
+        Addup_16.io.B_in := twoup_16.io.B_out
+        resultup_16  := Addup_16.io.sum
 
         result_32 := Cat(resultup_16,resultlow_16).asSInt
       }
     }
 
     io.result := result_32
+}
+
+
+object GenerateVerilog extends App {
+  (new chisel3.stage.ChiselStage).emitVerilog(new PExtALU(), Array("--target-dir", "generated"))
 }
